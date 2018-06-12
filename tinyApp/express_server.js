f99135a4108e7bb94af8aa2f698e3570eb402453
Eliza Moore <elzmoore13@gmail.com>
2018-06-12 21:39:01 +0000

    add submission page, new POST route, and generateRandomString function

--- tinyApp/express_server.js
+++ tinyApp/express_server.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
+
+//function to generate unique shortURLS
+function generateRandomString(){
+  const uniqueKey = Math.random().toString(36).replace('0.','').split('').slice(0,6).join('');
+  return uniqueKey;
+}
+
+//GET method routes
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -16,6 +26,10 @@ app.get("/", (req, res) => {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index.ejs', templateVars)
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new.ejs');
 })
 
 app.get('/urls/:id', (req, res) => {
@@ -32,11 +46,20 @@ app.get('/hello', (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
+//POST method routes
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listenign on port ${PORT}!`);
 });
 
 
 // var uid = function() {
-//   return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+// return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
 // }
